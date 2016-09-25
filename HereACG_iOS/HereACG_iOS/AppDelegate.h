428627aafc7419d26cc84ad64258f0b2578e315b
syy <yunyang088@gmail.com>
2016-09-25 15:33:55 +0800

    工程初始化
    添加第三方库，加入类目，建立pch文件

---
+++ HereACG_iOS/HereACG_iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HereACG_iOS
+//
+//  Created by 石云洋 on 2016/9/25.
+//  Copyright © 2016年 石云洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HereACG_iOS/HereACG_iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HereACG_iOS
+//
+//  Created by 石云洋 on 2016/9/25.
+//  Copyright © 2016年 石云洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
